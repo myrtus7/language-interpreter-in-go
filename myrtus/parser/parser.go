76aafe2ddb226e7925468c3cae45752f6603e26b
myrcraft <myrtgnt@gmail.com>
2026-09-03 12:23:14 +0200

    parsing return statements

--- myrtus/parser/parser.go
+++ myrtus/parser/parser.go
@@ -47,6 +47,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -87,4 +89,13 @@ func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
 		t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	for p.curToken.Type != token.SEMICOLON {
+		p.nextToken()
+	}
+	return stmt
 }
